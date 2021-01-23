aeb1d7c1e6f368035260a743bf6737257bc81f09
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-23 13:33:09 -0300

    test update

--- test/index.js
+++ test/index.js
@@ -5,7 +5,29 @@ require('../index').create(require('mysql'), 'default', require('./data/config.j
     .then(db => {
 
         // Print
-        console.log(db);
+        db.query('SELECT * FROM discord_users')
+
+            // Success
+            .then(data => {
+
+                // Print
+                console.log(data);
+
+                // Complete
+                return;
+
+            })
+
+            // Fail
+            .catch(err => {
+
+                // Print
+                console.error(err);
+
+                // Complete
+                return;
+
+            });
 
         // Complete
         return;
