cbfa2788cc9970ad90168053032a8f2350301ec5
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-23 13:26:48 -0300

    module update

--- index.js
+++ index.js
@@ -1,108 +1,133 @@
 // Modules
-module.exports = function (mysql, proxyType = null, databases, cfg) {
-    return new Promise(function (resolve, reject) {
+module.exports = {
 
-        // Modules
-        const util = require('util');
-        const clone = require('clone');
-        const objType = require('@tinypudding/puddy-lib/get/objType');
-        const _ = require('lodash');
-        let db = {};
-        let databaseList = [];
+    // Same User Generator
+    sameUser: function (database, login) {
 
-        // Create Settings
-        const tinyCfg = _.defaultsDeep({}, cfg, {
-            charset: "utf8mb4"
-        });
+        // Validator
+        if (typeof database === "string" && require('@tinypudding/puddy-lib/get/objType')(login, 'object')) {
 
-        // Convert
-        if (!Array.isArray(databases)) {
-            databaseList = [clone(databases)];
-        } else {
-            databaseList = clone(databases);
+            // Result
+            const result = require('clone')(login);
+            result.database = database;
+            return result;
+
         }
 
-        // Run Array
-        for (const item in databaseList) {
+        // Nope
+        else {
+            return null;
+        }
 
-            // Exist
-            if (objType(databaseList[item], 'object') && objType(databaseList[item].data, 'object')) {
+    },
 
-                // Charset
-                if (typeof databaseList[item].data.charset !== "string") {
-                    databaseList[item].data.charset = tinyCfg.charset;
-                }
+    // Create DB Value
+    create: function (mysql, proxyType, databases, cfg) {
+        return new Promise(function (resolve, reject) {
 
-                // Exist Proxy
-                if (typeof proxyType === "string" && proxyType !== "default") {
+            // Modules
+            const util = require('util');
+            const clone = require('clone');
+            const objType = require('@tinypudding/puddy-lib/get/objType');
+            const _ = require('lodash');
+            let db = {};
+            let databaseList = [];
 
-                    // Google Cloud
-                    if (
+            // Create Settings
+            const tinyCfg = _.defaultsDeep({}, cfg, {
+                charset: "utf8mb4"
+            });
 
-                        // Validate Type
-                        (
-                            (proxyType === "firebase" && !require('@tinypudding/puddy-lib/firebase/isEmulator')()) ||
-                            proxyType === "google_cloud"
-                        ) && 
-                        
-                        // Var
-                        objType(ddatabaseList[item].google_cloud, 'object') &&
-                        typeof databaseList[item].google_cloud.socketPath === "string"
+            // Convert
+            if (!Array.isArray(databases)) {
+                databaseList = [clone(databases)];
+            } else {
+                databaseList = clone(databases);
+            }
 
-                    ) {
+            // Run Array
+            for (const item in databaseList) {
 
-                        // Insert the config
-                        databaseList[item].data.socketPath = databaseList[item].google_cloud.socketPath;
-                    
+                // Exist
+                if (objType(databaseList[item], 'object') && objType(databaseList[item].data, 'object')) {
+
+                    // Charset
+                    if (typeof databaseList[item].data.charset !== "string") {
+                        databaseList[item].data.charset = tinyCfg.charset;
                     }
 
-                    // Nothing
-                    else { reject(new Error('Invalid Proxy Type! Index:' + item)); return; }
+                    // Exist Proxy
+                    if (typeof proxyType === "string" && proxyType !== "default") {
+
+                        // Google Cloud
+                        if (
+
+                            // Validate Type
+                            (
+                                (proxyType === "firebase" && !require('@tinypudding/puddy-lib/firebase/isEmulator')()) ||
+                                proxyType === "google_cloud"
+                            ) &&
+
+                            // Var
+                            objType(ddatabaseList[item].google_cloud, 'object') &&
+                            typeof databaseList[item].google_cloud.socketPath === "string"
+
+                        ) {
+
+                            // Insert the config
+                            databaseList[item].data.socketPath = databaseList[item].google_cloud.socketPath;
+
+                        }
+
+                        // Nothing
+                        else { reject(new Error('Invalid Proxy Type! Index:' + item)); return; }
+
+                    }
+
+                    // Normal Connection
+                    else {
+
+                        // SSL Files
+                        if (objType(databaseList[item].default.ssl, 'object')) { databaseList[item].data.ssl = databaseList[item].default.ssl; }
+
+                        // Host and Port
+                        databaseList[item].data.host = databaseList[item].default.host;
+                        databaseList[item].data.port = databaseList[item].default.port;
+
+                    }
+
+                    // Create DB Connection
+
+                    // Complete
+                    try {
+                        db[databaseList[item].data.database] = mysql.createPool(databaseList[item].data);
+                        db[databaseList[item].data.database].query = util.promisify(db[databaseList[item].data.database].query);
+                    }
+
+                    // Error
+                    catch (err) {
+                        reject(err);
+                        return;
+                    }
 
                 }
 
-                // Normal Connection
-                else {
-
-                    // SSL Files
-                    if (objType(databaseList[item].default.ssl, 'object')) { databaseList[item].data.ssl = databaseList[item].default.ssl; }
-
-                    // Host and Port
-                    databaseList[item].data.host = databaseList[item].default.host;
-                    databaseList[item].data.port = databaseList[item].default.port;
-
-                }
-
-                // Create DB Connection
-
-                // Complete
-                try {
-                    db[databaseList[item].data.database] = mysql.createPool(databaseList[item].data);
-                    db[databaseList[item].data.database].query = util.promisify(db[databaseList[item].data.database].query);
-                }
-
-                // Error
-                catch (err) {
-                    reject(err);
-                    return;
-                }
+                // Nope
+                else { reject(new Error('Invalid MySQL Setting! Index:' + item)); return; }
 
             }
 
-            // Nope
-            else { reject(new Error('Invalid MySQL Setting! Index:' + item)); return; }
+            // Fix
+            const dbKets = Object.keys(db);
+            if (dbKets.length < 2) {
+                db = db[dbKets[0]];
+            }
 
-        }
+            // Complete
+            resolve(db);
+            return;
 
-        // Fix
-        const dbKets = Object.keys(db);
-        if (dbKets.length < 2) {
-            db = db[dbKets[0]];
-        }
+        });
+    }
 
-        // Complete
-        resolve(db);
-        return;
-
-    });
 };
